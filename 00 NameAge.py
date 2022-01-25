53c34872a5ade6c6b9d58f1d868d620ef7ed90f9
lizhy2010 <52873172+lizhy2010@users.noreply.github.com>
2022-01-25 18:55:58 +0800

    Update 00 NameAge.py

--- 00 NameAge.py
+++ 00 NameAge.py
@@ -1,6 +1,6 @@
 name = input("Hello, what is your name? ")
 age = input("How old are you? ")
 
-year = (100 - int(age)) + 2020 
+year = (100 - int(age)) + 2022
 
 print("Hello " + name + ", you will turn 100 years old in year " + str(year));
